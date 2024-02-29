b00ed88d10ad069ed89db3efd03898927f4be87d
user-11150 <13873387+user-11150@user.noreply.gitee.com>
2024-02-29 21:45:58 +0800

    <优化> 优化了刚开始会出现什么也没有过一会才会出现内容的情况，减少了加载的时间

--- server/core.py
+++ server/core.py
@@ -10,11 +10,11 @@ from server.constants import IP
 # logstart
 from server.no_important.logstart import logstart
 
-import multiprocessing
-
-import asyncio
-
+# Feb.29[User-11150]: <优化> 虽然总的时间没有变化，甚至还可能增加
+#     但是，在函数内部导入可以使 “loading...” 提示语尽早出现，用户
+#     体验相对更好
 def run_app() -> None:
+    import asyncio
     async def _run_app():
         event = asyncio.Event()
         app = Application(urls) # type: ignore
@@ -22,14 +22,15 @@ def run_app() -> None:
         await event.wait()
     
     def chilren_process_execute_async_task(fn: Callable[[],asyncio.Condition]) -> None:
+        import multiprocessing
         def _wrapped_task():
             asyncio.set_event_loop(asyncio.new_event_loop())
             asyncio.run(fn())
         process = multiprocessing.Process(target=_wrapped_task)
         process.start()
-    
     chilren_process_execute_async_task(_run_app)
     logstart(IP,PORT)
 
 def main() -> None:
+    print('Loading...')
     run_app()
