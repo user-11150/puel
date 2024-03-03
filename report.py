4b21fb27f6f4e597cb68f94a9c7cdb12a81c2060
user-11150 <13873387+user-11150@user.noreply.gitee.com>
2024-03-03 13:31:11 +0800

    0.2.7-alpha.1

--- report.py
+++ report.py
@@ -12,40 +12,48 @@ sys.setrecursionlimit(1000000)
 def contain(p):
     return (".git" in p or ".obsidian" in p or "mypy_cache" in p)
 
-def getsumsize(p = path):
-    if contain(p):
-        return 0
-    if os.path.isdir(p):
-        result = 0
-        for i in os.listdir(p):
-            allpath = os.path.join(p,i)
-            result += getsumsize(allpath)
-        return result
-    else:
-        #print(p)
-        return os.path.getsize(p)    
+#def getsumsize(p = path):
+
+#    if os.path.isdir(p):
+#        if contain(p):
+#            return 0
+#        result = 0
+#        for i in os.listdir(p):
+#            allpath = os.path.join(p,i)
+#            result += getsumsize(allpath)
+#        return result
+#    else:
+#        #print(p)
+#        return os.path.getsize(p)
 
 def getfilename(path,end):
     lis = []
-    for root,dirs,files in os.walk(path):
+    for root,_,files in os.walk(path):
         for i in files:
             p = os.path.join(root,i)
+            #if contain(root):
+#                break
             if contain(p):
                 continue
             for i in end:
                 if p.endswith(i):
                     lis.append(p)
+                    break
     return lis
 @lru_cache()    
 def getLine(path):
-    try:
-        with open(path,'rt') as fp:
-            content = fp.read()
-            return content.count('\n') + 1
-    except:
-        return 1
+#    try:
+#        with open(path,'rt') as fp:
+#            content = fp.read()
+#            return content.count('\n') + 1
+#    except:
+#        return 1
+     with open(path,"rb") as fp:
+         content = fp.read()
+         return content.count(b"\n") + 1
 
-import threading
+from concurrent.futures import ThreadPoolExecutor
+from threading import Lock
 item = [
     (
         '代码',
@@ -151,41 +159,45 @@ def rmpycache():
             os.rmdir(p)
 
 def update(*args,**kwargs):
-    print(*args,**kwargs,file=stream)
+    print(*args,**kwargs)
 
 def getlength(i):
-    with open(i,'rb')as f:
-        res = f.read()
-    return len(res)
+    return os.path.getsize(i)
+
+pool = ThreadPoolExecutor(max_workers=10)
+mutex = Lock()
 def task(name,s):
-    update(f'{name}：')
-    f = files = getfilename(path,s)
-    
-    size = sum([os.path.getsize(i) for i in f])
-    line_n = sum([getLine(i) for i in f])
-    file_number = len(files)
-    if file_number == 0 or line_n == 0:
-        update('\t无')
-        return
-    def sizeof(x):
-        sss = 0.8
-        l = 5
-        def f(x): return str(round(x,3)).rjust(l,"0")
-        if x/1024/1024/1024 > sss:
-            return f'{f(x/1024/1024/1024)}GB'
-        if x/1024/1024 > sss:
-            return f'{f(x/1024/1024)}MB'
-        if x/1024 > sss:
-            return f'{f(x/1024)}KB'
-        return f'{x}字节'    
-    update('\t所有代码总大小',sizeof(size))
-    update('\t文件个数',len(files))
-    update('\t行数和',line_n)
-    update('\t平均文件行数',line_n/file_number)
-    update('\t平均每一个文件的大小',sizeof(size/len(files)))
-    update('\t平均一行多少',sizeof(size/line_n))
-    update('\t占总数的', round(size/sumsize*100,7),'%')
-    update('\t行数占总行数的',line_n/line*100,"%")
+    def compete():
+        files = getfilename(path,s)
+        
+        size = sum([os.path.getsize(i) for i in files])
+        line_n = sum([getLine(i) for i in files])
+        file_number = len(files)
+        with mutex:
+            update(f'{name}：')
+            if file_number == 0 or line_n == 0:
+                update('\t无')
+                return
+            def sizeof(x):
+                sss = 0.8
+                l = 5
+                def f(x): return str(x).rjust(l,"0")
+                if x/1024/1024/1024 > sss:
+                    return f'{f(x/1024/1024/1024)}GB'
+                if x/1024/1024 > sss:
+                    return f'{f(x/1024/1024)}MB'
+                if x/1024 > sss:
+                    return f'{f(x/1024)}KB'
+                return f'{x}字节'    
+            update('\t所有代码总大小',sizeof(size))
+            update('\t文件个数',len(files))
+            update('\t行数和',line_n)
+            update('\t平均文件行数',line_n/file_number)
+            update('\t平均每一个文件的大小',sizeof(size/len(files)))
+            #update('\t平均一行多少',sizeof(size/line_n))
+            update('\t占总数的', size/sumsize*100,'%')
+            update('\t行数占总行数的',line_n/line*100,"%")
+    pool.submit(compete)
 
 def main():
 
@@ -195,17 +207,19 @@ def main():
     day = (time.time()-startTime)/60/60/24
     stream = io.StringIO()
     line = sum([getLine(i) for i in getfilename(path,item[0][1])])
-    sumsize = getsumsize()
+    sumsize = getfilename(path,item[0][1])
     
     print('开发天数',day)
     print('平均每天代码行数',line/day)
     print('不算pyc，git，obsidian(markdwon软件)产生的')
-
-    for i in tqdm.tqdm(item):
+    
+    for i in item:
         task(*i)
-    print(stream.getvalue())
-    print(flush=True)
+    def atexit_flush():
+        print(stream.getvalue())
+        print(flush=True)
+    import atexit
+    atexit.register(atexit_flush)
 
-#n = timeit.timeit(main,number=10)
-#print(n/10)
-main()
+n = timeit.timeit(main,number=1)
+print(n/1)
