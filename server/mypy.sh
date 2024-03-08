30d9a6de22e499f4262631c3239e1008dde5de40
user-11150 <13873387+user-11150@user.noreply.gitee.com>
2024-03-08 19:30:42 +0800

    合并mypy.sh

--- server/mypy.sh
+++ server/mypy.sh
@@ -1,1 +1,11 @@
-mypy main.py --strict
+server_mypy_cache_dirname="./mypy_cache/service"
+client_mypy_cache_dirname="./mypy_cache/client"
+
+echo "---------------------------"
+echo "Types for server's service"
+mypy main.py --strict --cache-dir=${server_mypy_cache_dirname}
+
+echo "---------------------------"
+echo "Types for server's client"
+
+mypy ./client/__main__.py --cache-dir=${client_mypy_cache_dirname}
