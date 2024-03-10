207d95e8e8e17c34561d669ed638577396cb9e9e
user-11150 <13873387+user-11150@user.noreply.gitee.com>
2024-03-10 13:39:54 +0800

    更改版本

--- server/main.py
+++ server/main.py
@@ -6,8 +6,9 @@ os.chdir(os.path.dirname(__file__))
 assert sys.version_info >= (3,8),TypeError('Python版本过低')
 
 from server.core import main
+from server.version import getVersion
 
-__version__ = "0.2.6"
+__version__: str = getVersion(__file__)
 
 if __name__ == "__main__":
     main()
---
+++ server/server/version.py
@@ -0,0 +1,44 @@
+"""
+获取PTS11150的版本号
+"""
+
+import os
+from typing import (Final,
+                    IO,
+                    )
+
+def getVersion(base: str) -> Final[str]:
+    """
+    Return the PTS11150's version
+    """
+    
+    # There's a file in dirname.
+    # Its name is version_file_name
+    # In the file is PTS11150's version
+    
+    dirname: Final[str] = os.path.dirname(os.path.dirname(base))
+    version_file_name: Final[str] = "version"
+    
+    # It is the version_file path
+    p: Final[str] = os.path.join(dirname,version_file_name)
+    
+    try:
+        # ORCR
+        # 1. Open
+        # 2. Read
+        # 3. Close
+        # 4. Return
+        
+        # 1. Open
+        ios: IO = open(p, "rt", encoding='UTF-8')
+        
+        # 2. read
+        version = ios.read()
+        
+        # 3. Close
+        ios.close()
+        
+        # 4. Return
+        return version
+    except Exception as e:
+        raise Exception('Cannot get the PTS11150 version') from e
