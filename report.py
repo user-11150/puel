b5000c83433c2b8f292e3eff18b3f52fa1bd72ad
user-11150 <13873387+user-11150@user.noreply.gitee.com>
2024-04-11 22:08:28 +0800

    [Code review] The report.py code review

--- report.py
+++ report.py
@@ -1,6 +1,9 @@
+#pylint:disable=W0718
 #pylint:disable=E0102
-import os,sys
+import os
+import sys
 import time
+import re
 
 try:
     import rich
@@ -13,10 +16,10 @@ except ImportError as e:
 from rich.console import Console
 from rich.table import Table
 
-console = Console()
-
 from functools import lru_cache
-
+from concurrent.futures import ThreadPoolExecutor
+from threading import Lock
+from concurrent.futures import Future
 import sys
 
 def getPath():
@@ -33,20 +36,6 @@ sys.setrecursionlimit(1000000)
 
 def contain(p):
     return (".git" in p or ".obsidian" in p or "mypy_cache" in p)
-
-#def getsumsize(p = path):
-
-#    if os.path.isdir(p):
-#        if contain(p):
-#            return 0
-#        result = 0
-#        for i in os.listdir(p):
-#            allpath = os.path.join(p,i)
-#            result += getsumsize(allpath)
-#        return result
-#    else:
-#        #print(p)
-#        return os.path.getsize(p)
 
 def getfilename(path,end):
     lis = []
@@ -74,11 +63,9 @@ def getLine(path):
          content = fp.read()
          return content.count(b"\n") + 1
 
-from concurrent.futures import ThreadPoolExecutor
-from threading import Lock
-item = [
+items = [
     (
-        '代码',
+        'Code',
         (
             '.js',
             '.css',
@@ -88,28 +75,28 @@ item = [
         )
     ),
     (
-        '文档',
+        'Documentation',
         (
             '.md',
             'LICENSE'
         )
     ),
     (
-        'python',
+        'Python',
         (
             '.py',
         )
     ),
     
     (
-        '配置',
+        'Configuration',
         (
             '.json',
             '.ini'
         )
     ),
     (
-        '前端',
+        'HTML CSS JS',
         (
             '.js',
             '.css',
@@ -117,7 +104,7 @@ item = [
         )
     ),
     (
-        '图片，视频，音频等',
+        'Binary assets',
         (
             
             '.png',
@@ -129,13 +116,13 @@ item = [
         )
     ),
     (
-        'sh',
+        'Shell',
         (
             '.sh',
         )
     ),
     (
-        '日志',
+        'Logs',
         (
             
             '.log',
@@ -147,98 +134,164 @@ item = [
 ]
 
 
-item.insert(0,
-  ("所有的",
+items.insert(0,
+  ("All",
     tuple(set(
-        (lambda x:[n for t in x for n in t])([i for z,i in item])
+        (lambda x:[n for t in x for n in t])([i for z,i in items])
     ))
   )
 )
+class Report:
+    def __init__(self, items):
+        self.start_time = time.mktime(time.strptime('2024-2-24 22:0:0','%Y-%m-%d %H:%M:%S'))
+        self.dev_days = (time.time()-self.start_time)/60/60/24
+        self.items = items
+        self.console = Console()
+        self.pool = ThreadPoolExecutor()
+        self.futures = None
+        tw = 80
+        try:
+            tw = os.get_terminal_size().columns
+        except Exception:
+            pass
+        self.terminal_width = tw
 
+    def remove_pycache_files(self):
+        """
+        为了方便，直接复制的原本的，所以看起来缩进较多
+        """
+        def rmpycache():
+            def getpaths(path):
+                result = []
+                for i in os.listdir(path):
+                    p = os.path.join(path,i)
+                    result.append(p)
+                    if os.path.isdir(p):
+                        result += getpaths(p)
+                return result
+            def filterpyc(n):
+                if os.path.split(n)[1] == '__pycache__' or n.endswith('.pyc'):
+                    return True
+                return False
+            ps = getpaths(path)
+            pyc = filter(filterpyc,ps)
+            for p in pyc:
+                if os.path.isfile(p):
+                    os.remove(p)
+            pyc,pyc2 = filter(filterpyc,ps),filter(filterpyc,ps)
+            
+            for p in pyc:
+                
+                if os.path.isdir(p):
+                    os.rmdir(p)
+        rmpycache()
 
-def rmpycache():
-    def getpaths(path):
-        result = []
-        for i in os.listdir(path):
-            p = os.path.join(path,i)
-            result.append(p)
-            if os.path.isdir(p):
-                result += getpaths(p)
-        return result
-    def filterpyc(n):
-        if os.path.split(n)[1] == '__pycache__' or n.endswith('.pyc'):
-            return True
-        return False
-    ps = getpaths(path)
-    pyc = filter(filterpyc,ps)
-    for p in pyc:
-        if os.path.isfile(p):
-            os.remove(p)
-    pyc,pyc2 = filter(filterpyc,ps),filter(filterpyc,ps)
-    
-    for p in pyc:
-        
-        if os.path.isdir(p):
-            os.rmdir(p)
+    def day(self):
+        self.console.print("Number of dev days: ", self.dev_days)
 
-def update(table, name, value):
-    # print(*args,**kwargs)
-    table.add_row(name,str(value))
+    def create_task(self, item):
+        [name, ends] = item
+        report_item = ReportItem(name, ends, self)
+        return report_item.report()
 
-def getlength(i):
-    return os.path.getsize(i)
+    def report(self):
+        starttime = time.time()
+        self.remove_pycache_files()
+        self.day()
+        self.futures = []
+        for item in self.items:
+            self.futures.append(self.create_task(item))
+        self.wait_for(self.futures)
+        endtime = time.time()
+        t = int((endtime - starttime) * 1000) #   ↓ Used. pylint: disable=C0103
+        self.console.print(f"Time of this report {t} MS")
 
-pool = ThreadPoolExecutor(max_workers=10)
-mutex = Lock()
-def task(name,s):
-    def compete():
-        files = getfilename(path,s)
-        
-        size = sum([os.path.getsize(i) for i in files])
-        line_n = sum([getLine(i) for i in files])
-        file_number = len(files)
-        with mutex:
-            if file_number == 0 or line_n == 0:
-               
+    @staticmethod
+    def wait_for(fs: list[Future]):
+        for f in fs:
+            f.result()
+
+class ReportItem:
+    def __init__(self, name, ends, report: Report):
+        self.name = name
+        self.ends = ends
+        self.__report = report
+        self.table = None
+
+    def report(self):
+        def _report():
+            m = 1.1
+            self.table = Table(title=self.name, width=int(self.__report.terminal_width // m))
+            self.table.add_column("Name", justify="left")
+            self.table.add_column("Value", justify="center")
+            class Print(Exception):
+                pass
+            class Return(Exception):
+                pass
+            try:
+                files = getfilename(path, self.ends)
+                res_file_number = len(files)
+                if res_file_number == 0:
+                    raise Return()
+                size_i = sum(self.get_file_size(f) for f in files)
+                size = self.stringSize(size_i)
+                line_number_i = sum(getLine(f) for f in files)
+                line_number = self.seg(str(line_number_i))
+                average_number_of_rows = self.seg(int(line_number_i / res_file_number))
+                average_number_of_size = self.stringSize(size_i / res_file_number)
+                
+                self.table.add_row("Number of files", str(res_file_number))
+                self.table.add_row("Size", size)
+                self.table.add_row("Line number", line_number)
+                self.table.add_row("Average number of rows", average_number_of_rows)
+                self.table.add_row("Average number of size", average_number_of_size)
+                
+                raise Print()
+            except Print:
+                self.__report.console.print(self.table)
+            except Return:
                 return
-            tb = Table(title=name)
-            tb.add_column('属性名')
-            tb.add_column('值')
-            def sizeof(x):
-                sss = 0.8
-                l = 5
-                def f(x): return str(x).rjust(l,"0")
-                if x/1024/1024/1024 > sss:
-                    return f'{f(x/1024/1024/1024)}GB'
-                if x/1024/1024 > sss:
-                    return f'{f(x/1024/1024)}MB'
-                if x/1024 > sss:
-                    return f'{f(x/1024)}KB'
-                return f'{x}字节'    
-            update(tb, '所有代码总大小',sizeof(size))
-            update(tb, '文件个数',len(files))
-            update(tb, '行数和',line_n)
-            update(tb, '平均文件行数',line_n/file_number)
-            update(tb, '平均每一个文件的大小',sizeof(size/len(files)))
-            console.print(tb)
-    pool.submit(compete)
+            
+        return self.__report.pool.submit(_report)
+
+    @staticmethod
+    def get_file_size(i):
+        return os.path.getsize(i)
+
+    @staticmethod
+    def stringSize(byte: int):
+        base_unit = 'B', 1
+        unit_kb = "KB", 1024
+        unit_mb = "MB", 1024 * 1024
+        unit_gb = "GB", 1024 * 1024 * 1024
+        
+        current_unit = base_unit
+        
+        u = 0.85
+                
+        if byte > 1024 * u:
+            current_unit = unit_kb
+        if byte > 1024 * 1024 * u:
+            current_unit = unit_mb
+        if byte > 1024 * 1024 * 1024 * u:
+            current_unit = unit_gb
+        left = round(byte / current_unit[1], 2)
+        return f"{left}{current_unit[0]}"
+
+    @staticmethod
+    def seg(number: int):
+        """
+        example:
+            104354 => 10,4354
+        """
+        if type(number) is float or type(number) is int: # pylint: disable=C0123
+            number = str(number)
+        return re.sub(r"(?=\B\d{3}+$)", ",", str(number))
 
 def main():
-
-    rmpycache()
-    global line,sumsize,stream,day
-    startTime = time.mktime(time.strptime('2024-2-24 22:0:0','%Y-%m-%d %H:%M:%S'))
-    day = (time.time()-startTime)/60/60/24
-    line = sum([getLine(i) for i in getfilename(path,item[0][1])])
-    sumsize = getfilename(path,item[0][1])
+    report = Report(items)
+    report.report()
     
-    #    print('开发天数',day)
-#        print('平均每天代码行数',line/day)
-#        print('不算pyc，git，obsidian(markdwon软件)产生的')
-#        
-    console.print('开发天数', day, '天')
-    for i in item:
-        task(*i)
 
 if __name__ == "__main__":
     main()
