9ed97329f6299ae823faaf046f7f50ea67c08237
user-11150 <13873387+user-11150@user.noreply.gitee.com>
2024-04-27 18:58:01 +0800

    [C] Web

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -6,19 +6,23 @@ $(function() {
 function curser(){
   const $curser = $("<img>")
                    .css("position", "absolute")
+                   .css("top", "-200px")
+                   .css("left", "-200px")
                    .css("width", "32px")
                    .css("border-radius", "50%")
-                   .css("display", "none")
                    .attr("src", '/imgs/curser.jpg')
                    .appendTo(document.body)
   
   const curser = $curser[0]
   
-  document.ontouchmove = function(e){
-    $curser.css("display", "block")
+  document.onmousemove = function(e){
     $curser.css("top", e.pageY - curser.clientHeight / 2)
     $curser.css("left", e.pageX - curser.clientWidth / 2)
   }
 
-  document.onmousemove = document.ontouchmove
+  document.ontouchmove = function(e){
+    for(const touch of e.touches){
+      this.onmousemove(touch)
+    }
+}
 }
