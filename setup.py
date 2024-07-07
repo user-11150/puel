27a2e69ce325e773d253b54c4d08c2b78ad0a8ed
user-11150 <ck12_2022@qq.com>
2024-07-07 08:56:00 +0800

    [remove] remove obfuscate

--- setup.py
+++ setup.py
@@ -10,11 +10,10 @@ Let's compile UEL.
 
 try:
     import Cython as _
-    import python_minifier as _
 except:
     import pip
     
-    pip.main(["install", "Cython", "python-minifier"])
+    pip.main(["install", "Cython"])
 
 from Cython.Build import cythonize
 
@@ -25,8 +24,6 @@ from setuptools import Extension
 from setuptools import find_namespace_packages
 
 from setuptools.command.build_ext import build_ext
-from setuptools._distutils import file_util
-from setuptools._distutils.errors import DistutilsFileError
 
 from setuptools.dist import Distribution
 
@@ -34,57 +31,8 @@ import os
 import sys
 import re
 import platform
-import contextlib
-import io
-import python_minifier
 
-def copy_file_contents(src, dst, buffer_size=16 * 1024):  # noqa: C901
-    """Copy the file 'src' to 'dst'; both must be filenames.  Any error
-    opening either file, reading from 'src', or writing to 'dst', raises
-    DistutilsFileError.  Data is read/written in chunks of 'buffer_size'
-    bytes (default 16k).  No attempt is made to handle anything apart from
-    regular files.
-    """
-    # Stolen from shutil module in the standard library, but with
-    # custom error-handling added.
-    def _handler(data, file):
-        if file.endswith(".py"):
-            if "nominify" in os.path.split(file)[0]:
-                return data
-            return python_minifier.minify(data.decode(),
-                remove_literal_statements=True,
-                rename_globals=True).encode()
-        return data
-    
-    fsrc = None
-    fdst = None
-    try:
-        try:
-            fsrc = open(src, 'rb')
-        except OSError as e:
-            raise DistutilsFileError(f"could not open '{src}': {e.strerror}")
-
-        if os.path.exists(dst):
-            try:
-                os.unlink(dst)
-            except OSError as e:
-                raise DistutilsFileError(f"could not delete '{dst}': {e.strerror}")
-
-        try:
-            fdst = open(dst, 'wb')
-        except OSError as e:
-            raise DistutilsFileError(f"could not create '{dst}': {e.strerror}")
-        
-        src_data = _handler(fsrc.read(), src)
-        fdst.write(src_data)
-    
-    finally:
-        if fdst:
-            fdst.close()
-        if fsrc:
-            fsrc.close()
-
-with open("src/uel/nominify/version.py", "rt", encoding="utf8") as f:
+with open("src/uel/version.py", "rt", encoding="utf8") as f:
     version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
 
@@ -191,13 +139,6 @@ def get_extensions():
 
     return extensions
 
-def is_minify():
-    if len(sys.argv[1:]) > 1:
-        return True
-    if "dist" in sys.argv[1]:
-        return True
-    return False
-
 
 kwargs = {
     "install_requires": ["objprint"]
@@ -212,9 +153,6 @@ check_environment()
 
 if is_building():
     kwargs["ext_modules"] = get_extensions()
-
-if is_minify():
-    file_util._copy_file_contents = copy_file_contents
 
 setup(name = "uel",
     version = version,
